28703710a81bf338b5dd2e1dcd324e96481f9c77
Bennici <alexandre-ben@hotmail.fr>
2023-11-28 09:56:41 +0100

    update infobox style

--- server.R
+++ server.R
@@ -95,15 +95,15 @@ server <- function(input, output,session) {
     tagList(
       fluidRow(
         div(class = "row",
-          class = "col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2",
+          class = "col-xs-4 col-sm-6 col-md-4 col-lg-2 col-xl-2",
           uiOutput("entities_selector_s")
         ),
         div(
-          class = "col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2",
+          class = "col-xs-4 col-sm-6 col-md-4 col-lg-2 col-xl-2",
           uiOutput("stat_selector_s")
         ),
         div(
-          class = "col-12 col-sm-6 col-md-4 col-lg-2 col-xl-2",
+          class = "col-xs-4 col-sm-6 col-md-4 col-lg-2 col-xl-2",
           uiOutput("download_wrapper")
         )
       ),
