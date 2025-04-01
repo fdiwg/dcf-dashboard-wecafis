668b4fb80de1a6f7d393f324b66e97707d2b8e92
eblondel <emmanuel.blondel1@gmail.com>
2025-04-01 16:18:39 +0200

    ensure vrule (remote) is loaded

--- app.R
+++ app.R
@@ -5,6 +5,7 @@ options(stringsAsFactors = FALSE)
 #packages
 #---------------------------------------------------------------------------------------
 library(dcf.dashboard)
+library(vrule) #explicit call to vrule (not on CRAN) to reference it to renv.lock
 
 # Run the application 
 #---------------------------------------------------------------------------------------
